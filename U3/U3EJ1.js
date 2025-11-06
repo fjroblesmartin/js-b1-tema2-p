3662d0f6cebd7e901550ab9f2602a992e41428f6
Javier <fjrobles@gmail.com>
2025-11-06 23:24:20 +0100

    Update U3EJ1.js
    Arreglado

--- U3/U3EJ1.js
+++ U3/U3EJ1.js
@@ -8,15 +8,7 @@
   Por un lado, implementarle un setter (al que denominaremos carAntiguaty) que nos permita definir el valor de la propiedad "year"(año de fabricación)
    a partir de un parámetro recibido con información sobre los años de antigüedad del vehículo (podéis considerar directamente el año 2023 a efectos de cálculo).
   Por otro lado, implementarle un getter (al que denominaremos getCarDescription) que nos permita obtener la marca, modelo y cilidrada del vehículo directamente.
-  
-  	
-  -- CATALÀ
-  -- EXERCICI 2.3.1 ENUNCIAT:
-  Donada la definició de la classe Car que representarà els cotxes d'un concessionari (vista ja a l'exercici 2.2.1 d'aquest mateix Tema 2),
-  se'ns demana ampliar la seva definició de la manera següent:
-  D'una banda, implementar-li un setter (que anomenarem carAntiguaty) que ens permeti definir el valor de la propietat "year" (any de fabricació)
-   a partir d'un paràmetre rebut amb informació sobre els anys d'antiguitat del vehicle (podeu considerar directament l'any 2023 a efectes de càlcul).
-  D'altra banda, implementar-li un getter (que anomenarem getCarDescription) que ens permeti obtenir la marca, model i cilidrada del vehicle directament.
+      
 */
 /**
 * Do not uncomment this class, this is just informative to show the structure of the class
@@ -33,7 +25,22 @@ class Car {
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
 
+class Car {
+  constructor(brand, model, displacement, horsePower, year) {
+    this.brand = brand;
+    this.model = model;
+    this.displacement = displacement;
+    this.horsePower = horsePower;
+    this.year = year;
+  }
 
+  set carAntiguaty(anyo){
+    this.year =2023-anyo;
+  }
+  get getCarDescription(){
+    return `${this.brand} ${this.model} ${this.displacement}cc`  
+  }
+}
 
 
 
