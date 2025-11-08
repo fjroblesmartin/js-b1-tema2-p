076079e643689af760a0985e49b1b04cc50456d0
Javier <fjrobles@gmail.com>
2025-11-08 19:07:10 +0100

    Update U6EJ2.js
    Hecho

--- U6/U6EJ2.js
+++ U6/U6EJ2.js
@@ -10,20 +10,32 @@
   que tienen una antiguedad inferior a 25 años.
 	- Pasar este nuevo Array a un texto plano JSON en una nueva constante denominada newCarsJSON.
 	
-  
-  -- CATALÀ
-  -- EXERCICI 2.6.2 ENUNCIAT:
-	Donat el valor de la constant anomenada myJSON, un text pla en format JSON que conté la descripció d'un Array amb tres objectes de la classe Car,
-  vista a l'exercici 2.5.2. Se'ns demana:
-	- Definir en una constant anomenada myCars un Array d'objectes Car a partir d'aquest text.
-	- Obtenir un nou Array en una altra constant (a la qual anomenarem lessThan25) tots els cotxes definits a l'Array anterior (myCars) 
-  que tenen una antiguitat inferior a 25 anys.
-	- Passar aquest nou Array a un text pla JSON en una nova constant anomenada newCarsJSON.
 */
 
 const myJSON= '[{"brand":"Ford","model":"Mustang Boss 429","displacement":7000,"horsePower":375,"year":1969},{"brand":"BMW","model":"520d","displacement":1980,"horsePower":136,"year":2002},{"brand":"Mercedes-Benz","model":"280 SLC","displacement":2746,"horsePower":136,"year":1979}]';
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+
+const myCars = JSON.parse(myJSON);
+
+console.log("1. Array de coches (myCars):", myCars);
+
+
+/**
+ * Filtra el array myCars.
+ * Condición: (currentYear - car.year) < 25
+ */
+
+const lessThan25 = myCars.filter(car => {
+    const age = 2025 - car.year;
+    return age < 25;
+});
+
+console.log(`\n2. Coches con menos de 25 años :`, lessThan25);
+
+const newCarsJSON = JSON.stringify(lessThan25);
+
+console.log("\n3. Texto JSON resultante (newCarsJSON):", newCarsJSON);
 
 
 
